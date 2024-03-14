cbe91aa8349198ac95331e79c2d58960a235d885
YutaNakamura <yu.nakamura@zenplace.co.jp>
2024-03-14 15:45:02 +0900

    add geo

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -4,6 +4,7 @@ export default function Layout({children}: {children: React.ReactNode;}) {
   return <>
     <nav>
       <li><Link href={`/about`}>About</Link> </li>
+      <li><Link href={`/geo`}>Geo</Link> </li>
     </nav>
     {children}
   </>
